20b35337af937f6e2a4d11b2c63417264125303f
ferdelamad <fernando.delamadrid@checkr.com>
2019-04-16 09:30:28 -0700

    Add create book form

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_16_051305) do
+ActiveRecord::Schema.define(version: 2019_04_16_152020) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
-    t.string "description"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
